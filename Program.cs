01bf691509f50260f9d960a3c162aaa29bbec70e
Joseph <joejuszak@gmail.com>
2026-09-19 13:23:03 -0400

    Add header and introduction lines

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+*Name: Joseph Juszak
+*Course: CSCI 1250 Section 001
+*Assignment: Lab 0, Hello World
+*Date: September 19, 2026
+*Description: Prints a greeting and a short introduction to the console.
+*/
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Joseph Juszak");
+Console.WriteLine("This is my first program at ETSU");
